5abc7dbdf01defca8722df72c18e2a94bf3b2ed4
Ben Sperry <bsper2@illinois.edu>
2022-04-29 15:18:40 -0500

    SVC-12072 Add custom fact for manage_repos

---
+++ lib/facter/rhsm_manage_repo.rb
@@ -0,0 +1,15 @@
+Facter.add('rhsm_manage_repo') do
+  confine osfamily: 'RedHat'
+  setcode do
+    rhsm_result = Facter::Util::Resolution.exec('subscription-manager config --list  | grep manage_repos | awk \'{ print $NF}\'')
+    case rhsm_result
+    when '0', '[0]'
+      false
+    when '1', '[1]'
+      true
+    else
+      Facter.warn('Problem detecting rhsm_manage_repo')
+      'Error'
+    end
+  end
+end
